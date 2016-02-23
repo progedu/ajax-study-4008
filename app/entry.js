09d9834c40c2fd27f42885acb4fae74bd82b8325
Soichiro-Yoshimura <yoshimura@soichiro>
2016-02-23 14:15:53 +0900

    first commit

---
+++ app/entry.js
@@ -0,0 +1,16 @@
+'use strict';
+let $ = require('jquery');
+let block = $('#block');
+let scalingButton = $('#scaling-button');
+
+scalingButton.click(() => {
+  block.animate({ width: '200pt', height: '200pt' }, 2000);
+  block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+let movingButton = $('#moving-button');
+
+movingButton.click(() => {
+  block.animate({ 'marginLeft': '500px' }, 500);
+  block.animate({ 'marginLeft': '20px' }, 1000);
+});
