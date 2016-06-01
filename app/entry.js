2c3b1c7e3e6d784c54451eb1be2f6945488ef5f3
Yuya Takashina <yuya_takashina@dwango.co.jp>
2016-06-01 18:21:22 +0900

    let to consssssssssssssstt!!!!!

--- app/entry.js
+++ app/entry.js
@@ -1,14 +1,14 @@
 'use strict';
-let $ = require('jquery');
-let block = $('#block');
-let scalingButton = $('#scaling-button');
+const $ = require('jquery');
+const block = $('#block');
+const scalingButton = $('#scaling-button');
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
 
-let movingButton = $('#moving-button');
+const movingButton = $('#moving-button');
 
 movingButton.click(() => {
   block.animate({ 'marginLeft': '500px' }, 500);
